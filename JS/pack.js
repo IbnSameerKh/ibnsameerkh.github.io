d3951550982c315ffb82d2ea69e62801924942dc
IbnSameerKh <93059671+IbnSameerKh@users.noreply.github.com>
2022-10-02 01:45:47 +0300

    Add files via upload

---
+++ JS/pack.js
@@ -0,0 +1,321 @@
+function open1() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function close1() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+function open2() {
+  document.getElementById("mySidebar2").style.display = "block";
+  document.getElementById("myOverlay2").style.display = "block";
+}
+
+function close2() {
+  document.getElementById("mySidebar2").style.display = "none";
+  document.getElementById("myOverlay2").style.display = "none";
+}
+
+function AccF(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("show") == -1) {
+    x.className += " show";
+   x.previousElementSibling.className += " cb ";
+  } else { 
+    x.className = x.className.replace(" show"," ");
+   x.previousElementSibling.className = x.previousElementSibling.className.replace(" cb ", " ");
+  }
+}
+
+function AccF2(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("show") == -1) {
+    x.className += " show";
+  } else { 
+    x.className = x.className.replace(" show", "");
+  }
+}
+
+
+//this code for moving words
+
+var ml4 = {};
+ml4.opacityIn = [0,1];
+ml4.scaleIn = [0.8, 1];
+ml4.scaleOut = 0.5;
+ml4.durationIn = 500;
+ml4.durationOut = 400;
+ml4.delay = 500;
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml4 .letters-1',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-1',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: 1000
+  }).add({
+    targets: '.ml4 .letters-4',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-4',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-5',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-5',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-6',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-6',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4',
+    opacity: 0,
+    duration: 500,
+    delay: 500
+  });
+
+
+
+/*--------------------
+Vars
+--------------------*/
+const deg = (a) => Math.PI / 180 * a
+const rand = (v1, v2) => Math.floor(v1 + Math.random() * (v2 - v1))
+const opt = {
+  particles: window.width / 500 ? 1000 : 500,
+  noiseScale: 0.009,
+  angle: Math.PI / 180 * -90,
+  h1: rand(0, 360),
+  h2: rand(0, 360),
+  s1: rand(20, 90),
+  s2: rand(20, 90),
+  l1: rand(30, 80),
+  l2: rand(30, 80),
+  strokeWeight: 1.2,
+  tail: 82,
+}
+const Particles = []
+let time = 0
+document.body.addEventListener('click', () => {
+  opt.h1 = rand(0, 360)
+  opt.h2 = rand(0, 360)
+  opt.s1 = rand(20, 90)
+  opt.s2 = rand(20, 90)
+  opt.l1 = rand(30, 80)
+  opt.l2 = rand(30, 80)
+  opt.angle += deg(random(60, 60)) * (Math.random() > .5 ? 1 : -1)
+  
+  for (let p of Particles) {
+    p.randomize()
+  }
+})
+
+
+/*--------------------
+Particle
+--------------------*/
+class Particle {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+    this.lx = x
+    this.ly = y
+    this.vx = 0
+    this.vy = 0
+    this.ax = 0
+    this.ay = 0
+    this.hueSemen = Math.random()
+    this.hue = this.hueSemen > .5 ? 20 + opt.h1 : 20 + opt.h2
+    this.sat = this.hueSemen > .5 ? opt.s1 : opt.s2
+    this.light = this.hueSemen > .5 ? opt.l1 : opt.l2
+    this.maxSpeed = this.hueSemen > .5 ? 3 : 2
+  }
+  
+  randomize() {
+    this.hueSemen = Math.random()
+    this.hue = this.hueSemen > .5 ? 20 + opt.h1 : 20 + opt.h2
+    this.sat = this.hueSemen > .5 ? opt.s1 : opt.s2
+    this.light = this.hueSemen > .5 ? opt.l1 : opt.l2
+    this.maxSpeed = this.hueSemen > .5 ? 3 : 2
+  }
+  
+  update() {
+    this.follow()
+    
+    this.vx += this.ax
+    this.vy += this.ay
+    
+    var p = Math.sqrt(this.vx * this.vx + this.vy * this.vy)
+    var a = Math.atan2(this.vy, this.vx)
+    var m = Math.min(this.maxSpeed, p)
+    this.vx = Math.cos(a) * m
+    this.vy = Math.sin(a) * m
+    
+    this.x += this.vx
+    this.y += this.vy
+    this.ax = 0
+    this.ay = 0
+    
+    this.edges()
+  }
+  
+  follow() {
+    let angle = (noise(this.x * opt.noiseScale, this.y * opt.noiseScale, time * opt.noiseScale)) * Math.PI * 0.5 + opt.angle
+    
+    this.ax += Math.cos(angle)
+    this.ay += Math.sin(angle)
+    
+  }
+  
+  updatePrev() {
+    this.lx = this.x
+    this.ly = this.y
+  }
+  
+  edges() {
+    if (this.x < 0) {
+      this.x = width
+      this.updatePrev()
+    }
+    if (this.x > width) {
+      this.x = 0
+      this.updatePrev()
+    }
+    if (this.y < 0) {
+      this.y = height
+      this.updatePrev()
+    }
+    if (this.y > height) {
+      this.y = 0
+      this.updatePrev()
+    }
+  }
+  
+  render () {
+    stroke(`hsla(${this.hue}, ${this.sat}%, ${this.light}%, .5)`)
+    line(this.x, this.y, this.lx, this.ly)
+    this.updatePrev()
+  }
+}
+
+
+/*--------------------
+Setup
+--------------------*/
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  for (let i = 0; i < opt.particles; i++) {
+    Particles.push(new Particle(Math.random() * width, Math.random() * height))
+  }
+  strokeWeight(opt.strokeWeight)
+}
+
+
+/*--------------------
+Draw
+--------------------*/
+function draw() {
+  time++
+  background(0, 100 - opt.tail)
+  
+  for (let p of Particles) {
+    p.update()
+    p.render()
+  }
+}
+
+
+/*--------------------
+Resize
+--------------------*/
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
+
+
+
+
+//+- section
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", 
+  function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+  );
+}
+
+
+
+
+
+
+
+
+ 
+ 
+ 
